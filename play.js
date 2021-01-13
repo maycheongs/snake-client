a16ad0c0bd1a1b2326a02ce95b7761a580777cc4
May C <maycheongs@gmail.com>
2021-01-13 19:10:53 +0000

    ctrl c quits stdin

--- play.js
+++ play.js
@@ -1,11 +1,24 @@
 const connect = require('./client');
 
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = stdin.on('data', key => {
+    if (key === '\u0003') process.exit()    
+  })
+  return stdin
+}
 
 
-const snake = connect('mc');
+
+//const snake = connect('mc');
 
 
-//snake.write('Move: up')
 
 
+
+setupInput()
