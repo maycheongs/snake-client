340f333396c8e33abc02d4068454cd491b019ad4
May C <maycheongs@gmail.com>
2021-01-13 18:34:08 +0000

    setup connection to server

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = () => {
+  const play = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  play.setEncoding('utf8');
+
+  play.on('data', msg => {
+    console.log(msg)
+  })
+
+  return play //a socket object
+}
+
+
+connect();
+
+
+
